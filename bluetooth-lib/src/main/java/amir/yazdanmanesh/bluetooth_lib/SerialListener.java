55d8b4ad74c8e7a8e3f1d877eaa9aca39d603054
amir yazdan <amir.yazdanmanesh@gmail.com>
2022-04-29 23:34:01 +0430

    added all files

---
+++ bluetooth-lib/src/main/java/amir/yazdanmanesh/bluetooth_lib/SerialListener.java
@@ -0,0 +1,8 @@
+package amir.yazdanmanesh.bluetooth_lib;
+
+public interface SerialListener {
+    void onSerialConnect      ();
+    void onSerialConnectError (Exception e);
+    void onSerialRead         (byte[] data);
+    void onSerialIoError      (Exception e);
+}
